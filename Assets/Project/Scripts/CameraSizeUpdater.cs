ede5137155fa9bdc6572359abf0fecfc958a3d85
KoheiNagura <k.nagura.p@gmail.com>
2023-03-22 15:37:27 +0900

    add : cameraのorthographicSizeを自動で調整するように

---
+++ Assets/Project/Scripts/CameraSizeUpdater.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera)), ExecuteInEditMode]
+public class CameraSizeUpdater : MonoBehaviour
+{
+    [SerializeField] private float baseHeight;
+    [SerializeField] private int pixelPerUnit;
+    private Camera camera;
+    private float currentHeight;
+
+    private void Awake()
+        => UpdateCameraSize();
+
+    private void Update()
+    {
+        if (Application.isPlaying) return;
+        UpdateCameraSize();
+    }
+
+    private void UpdateCameraSize()
+    {
+        if (Screen.height == currentHeight) return;
+        if (camera == null) camera = GetComponent<Camera>();
+        var orthographicSize = baseHeight / pixelPerUnit / 2 * (512 / 100);
+        currentHeight = Screen.height;
+        camera.orthographicSize = orthographicSize * (currentHeight / baseHeight);
+    }
+}
