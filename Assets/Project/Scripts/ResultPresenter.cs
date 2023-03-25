d4c73538697e99d388ec492abda57f3bd131c14e
KoheiNagura <k.nagura.p@gmail.com>
2023-03-25 15:43:32 +0900

    add : 仮でリザルトデータを流すように

---
+++ Assets/Project/Scripts/ResultPresenter.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+public class ResultPresenter : MonoBehaviour
+{
+    [SerializeField] private ResultView view;
+
+    private void Awake()
+    {
+        var stacks = new List<StackedつData>() {
+            new StackedつData(null, "テスト1", 5),
+            new StackedつData(null, "テスト1", 5),
+            new StackedつData(null, "テスト1", 5),
+            new StackedつData(null, "テスト2", 5),
+            new StackedつData(null, "テスト2", 5),
+            new StackedつData(null, "テスト3", 5),
+        };
+        var result = new GameResult(stacks, null);
+        SetupView(result);
+    }
+
+    private void SetupView(GameResult result)
+    {
+        view.SetTotalPt(result.TotalPt);
+        view.SetScreenShot(result.ScreenShot);
+        var stacks = result.Stacks
+            .GroupBy(i => i.FontFamily)
+            .Select(i => (i.Count(), i.First().Sprite, i.Key))
+            .OrderByDescending(i => i.Item1)
+            .ToList();
+        view.SetStackedList(stacks);
+    }
+}
